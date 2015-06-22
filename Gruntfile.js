ad778ecf904493d3123133d0f0e7501e4da14bba
Arne Boon <arneboon@gmail.com>
2015-06-22 17:42:38 +0200

    implementation of grunt, see updated read me for usage

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+/* NOTES
+ * https://github.com/sindresorhus/grunt-shell
+ * http://gruntjs.com/api/grunt.util#grunt.util.spawn
+ * http://gruntjs.com/creating-tasks#why-doesn-t-my-asynchronous-task-complete
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-shell');
+
+  function execute(async) {
+    var settings = {
+      source: '../customfeeds/doodle3d-firmware/src/',
+      destination: '/tmp/usr/share/lua/wifibox/',
+      user: 'root',
+      remoteip: '192.168.5.1',
+      hostname: 'wifibox'
+    };
+
+    var options = {
+      cmd: 'bin/scpsync',
+      grunt: false,
+      opts: {stdio: 'inherit'},
+      args: [
+        '-s', settings.source,
+        '-d', settings.destination,
+        '-u', settings.user,
+        '-i', settings.remoteip,
+        '-n', settings.hostname
+        ]
+    };
+
+    function callback(error, result, code) {
+      console.log('done');
+
+      if (error) {
+        console.log('error:', error);
+      }
+    }
+
+    /* http://gruntjs.com/api/grunt.util#grunt.util.spawn */
+    var child = grunt.util.spawn(options, callback);
+  }
+
+  grunt.registerTask('default', function() {
+    /* http://gruntjs.com/creating-tasks#why-doesn-t-my-asynchronous-task-complete */
+    var async = this.async();
+    execute(async);
+  });
+
+}
--- README.md
+++ README.md
@@ -1,17 +1,40 @@
 # scpsync
-node application that syncs a file tree using scp over ssh
+Node application that syncs a file tree using scp over ssh.
 
 # installation
-clone in folder openwrt-buildroot
+Clone the module and install it global manually.
 
 ```bash
 $ git clone https://github.com/Doodle3D/scpsync.git
 $ cd scpsync
-$ npm install
+$ npm install -g .
+```
+
+Or install through npm with the global option.
+
+```bash
+$ npm install scpsync -g
+```
+
+Run the following to see the scpsync help and verify the global install worked.
+
+```bash
+$ scpsync -h
+```
+
+# setup
+Copy the Gruntfile.js to the root folder of your node project.
+
+Adjust the settings variable to your situation. These settings are the arguments that are passed to scpsync.
+
+Create an ssh key on your host machine and apply this ssh command:
+```bash
+cat ~/.ssh/id_rsa.pub | ssh wifibox 'cat >> /etc/dropbear/authorized_keys'
 ```
 
 # usage
+In your project rool folder call grunt and the scpsync module keeps syncing.
+
 ```bash
-npm start
+$ grunt
 ```
-It looks for the `../customfeeds` relatively, which is one folder up.
